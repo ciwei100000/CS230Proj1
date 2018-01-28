8c07d136f3f9e16027a5400947c55e8964bd2a92
XUQING LIU <ciwei100000@gmail.com>
2018-01-28 08:43:07 -0800

    set sphere.cpp

--- sphere.cpp
+++ sphere.cpp
@@ -6,6 +6,38 @@
 bool Sphere::Intersection(const Ray& ray, std::vector<Hit>& hits) const
 {
     // TODO
+    vec3 v = ray.endpoint - center;
+    vec3 w = ray.direction;
+    Hit hit;
+    double D = dot(w,v)^2-dot(w,w)*(dot(v,v)-radius^2);
+    if (D > 0)
+    {
+        double t0 = -dot(w,v) - sqrt(D);
+        double t1 = -dot(w,v) + sqrt(D);
+        if (t1 < 0)
+        {
+            return false;
+        }
+        else if (t0 < 0)
+        {
+            hit.object = *this;
+            hit.t = 0;
+            hit.ray_exiting = false;
+            hits.push_back(hit);
+            hit.t = t1;
+            hit.ray_exiting = true;
+            hits.push_back(hit);
+        }
+        else{
+            hit.object = *this;
+            hit.t = t0;
+            hit.ray_exiting = false;
+            hits.push_back(hit);
+            hit.t = t1;
+            hit.ray_exiting = true;
+            hits.push_back(hit);
+        }
+    }
     return false;
 }
 
@@ -13,5 +45,6 @@ vec3 Sphere::Normal(const vec3& point) const
 {
     vec3 normal;
     // TODO: set the normal
+    normal = (point - center).normalized;
     return normal;
 }
